c1164cd6a539c40e8a5d8ba0d8726b114e50b70c
Meagan Ramey <meagan.ramey@yahoo.com>
2020-08-28 20:32:37 -0400

    Create_word_dict function is not passing tests, but the print_words function has passed. Now working on sorting by value to get the top words.

--- wordcount.py
+++ wordcount.py
@@ -25,29 +25,46 @@ should return a dictionary with words as keys, and their counts as values.
 
 # Your name, plus anyone who helped you with this assignment
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Meagan Ramey"
 
 import sys
 
 
 def create_word_dict(filename):
-    """Returns a word/count dict for the given file."""
-    # Your code here
-    return
+    result = {}
+    words = filename.split()
+    for i, w in enumerate(words):
+        words[i] = w.lower()
+    words.sort()
+    for w in words:
+        if w not in result:
+            result.setdefault(w, 1)
+        else:
+            result[w] = result[w] + 1
+    return result
 
 
 def print_words(filename):
     """Prints one per line '<word> : <count>', sorted
     by word for the given file.
     """
-    # Your code here
-    return
+    with open(filename) as f:
+        f_contents = f.read()
+    result = create_word_dict(f_contents)
+    for k, v in result.items():
+        print(k, ' : ', v, end='\n')
 
+
+def sorted_value(v):
+    return v[-1]
 
 def print_top(filename):
     """Prints the top count listing for the given file."""
-    # Your code here
-    return
+    with open(filename) as f:
+        f_contents = f.read()
+    result = create_word_dict(f_contents)
+    result.sort(key=sorted_value)
+    return result
 
 
 # This basic command line argument parsing code is provided and calls
